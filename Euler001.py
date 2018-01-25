23476b56f0ba90e89ef1c1fcc51e41e544c67ea0
Alaroff <8170857+alar0330@users.noreply.github.com>
2018-01-25 21:40:14 +0100

    switch to generator

--- Euler001.py
+++ Euler001.py
@@ -9,7 +9,7 @@ def euler001(m = 1000):
     Find the sum of all the multiples of 3 or 5 below 1000.
     """
     
-    return sum([x for x in range(m) if (x % 3 == 0) or (x % 5 == 0)])
+    return sum(x for x in range(m) if (x % 3 == 0) or (x % 5 == 0))
     
     
 print('Euler001 answer: {}'.format(euler001()))
